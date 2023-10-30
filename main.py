31027ad8223c3a1195c796a6b27e762b4552ee04
nTorre <nicoals.torriglia02@gmail.com>
2023-10-30 12:21:34 +0100

    Prima versione funzionante

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
-from telethon import TelegramClient, events
-import csv
+from telethon.sync import TelegramClient, utils, events
 
 # Carica le variabili d'ambiente dal file .env
 load_dotenv()
@@ -11,41 +10,30 @@ api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 phone_number = os.getenv("PHONE_NUMBER")
 
-# Crea un dizionario per mappare i canali di origine ai canali di destinazione
-channel_mapping = {}
-
-# Leggi i canali da un file CSV
-with open('connections.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        channel_mapping[row['channel_source']] = row['channel_destination']
-
+# Crea un dizionario per mappare gli ID dei canali di origine agli ID dei canali di destinazione
+channel_mapping = {
+    -1002022962931: -1002091907058,  # Sostituisci gli ID dei canali con i tuoi
+}
 
 # Funzione per inoltrare i messaggi da un canale all'altro
 async def forward_message(event):
     message = event.message
-    if not message.sender_id == event.chat_id:  # Evita l'inoltro di messaggi propri
-        channel_username_destination = channel_mapping.get(event.chat.username)
-        if channel_username_destination:
-            # Utilizza get_input_entity per ottenere l'entità del canale privato
-            destination_entity = await client.get_input_entity(channel_username_destination)
-            await event.message.forward_to(destination_entity)
-
+    print("Ciao")
+    channel_id_destination = channel_mapping.get(event.chat_id)
+    if channel_id_destination:
+        print(channel_id_destination)
+        await event.message.forward_to(channel_id_destination)
 
 async def main():
     async with TelegramClient(phone_number, api_id, api_hash) as client:
-        for channel_source, channel_destination in channel_mapping.items():
-            # Utilizza get_input_entity per ottenere l'entità del canale privato
-            source_entity = await client.get_input_entity(channel_source)
-            # Aggiungi un gestore di eventi per catturare i nuovi messaggi nel canale di origine
-            client.add_event_handler(forward_message, events.NewMessage(chats=source_entity))
+        # Aggiungi un gestore di eventi per catturare i nuovi messaggi nei canali di origine
+        for channel_id_source, channel_id_destination in channel_mapping.items():
+            client.add_event_handler(forward_message, events.NewMessage(chats=channel_id_source))
 
         # Avvia il client
         await client.run_until_disconnected()
 
-
 if __name__ == '__main__':
     import asyncio
-
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
