1c77f2188bb821b17e2373eb8c3d71ab7c6c7735
nTorre <nicoals.torriglia02@gmail.com>
2023-10-30 12:11:51 +0100

    added fetures

--- README.md
+++ README.md
@@ -9,21 +9,30 @@ pip install -r requirements.txt
 
 ## Usage 
 
-Modify the this [file](connections.csv).
+1. Modify `.env` file and add your information
+
+2. Create your keys here: https://my.telegram.org/auth?to=apps
+
+3. Modify the this [file](connections.csv). 
 It has to be like:
 ```text
-https://t.me/canale1,https://t.me/chat1
-https://t.me/242wex2,https://t.me/dsisdos
+channel_source,channel_destination
+canale1_username,chat2
+chat_username,canale2_username
 ```
-
-where the first is the copied, the second the target. Then
+where the first is the copied, the second the target. Do not change the first row. Attention to not have two channels with the same name
+5. Then
 ```shell
 python main.py
 ```
-
 the first time on the device it should ask the code sent by telegram.
+
+---
 
 If you want to run in background:
 ```shell
 nohup python main.py &
 ```
+
+## TODO
+Add a parameter on connections.csv that allows to send also old messages
--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import os
+from dotenv import load_dotenv
+from telethon import TelegramClient, events
+import csv
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Carica le variabili d'ambiente dal file .env
+load_dotenv()
+
+# Leggi le variabili d'ambiente
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+phone_number = os.getenv("PHONE_NUMBER")
+
+# Crea un dizionario per mappare i canali di origine ai canali di destinazione
+channel_mapping = {}
+
+# Leggi i canali da un file CSV
+with open('connections.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        channel_mapping[row['channel_source']] = row['channel_destination']
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Funzione per inoltrare i messaggi da un canale all'altro
+async def forward_message(event):
+    message = event.message
+    if not message.sender_id == event.chat_id:  # Evita l'inoltro di messaggi propri
+        channel_username_destination = channel_mapping.get(event.chat.username)
+        if channel_username_destination:
+            # Utilizza get_input_entity per ottenere l'entità del canale privato
+            destination_entity = await client.get_input_entity(channel_username_destination)
+            await event.message.forward_to(destination_entity)
 
 
-# Press the green button in the gutter to run the script.
+async def main():
+    async with TelegramClient(phone_number, api_id, api_hash) as client:
+        for channel_source, channel_destination in channel_mapping.items():
+            # Utilizza get_input_entity per ottenere l'entità del canale privato
+            source_entity = await client.get_input_entity(channel_source)
+            # Aggiungi un gestore di eventi per catturare i nuovi messaggi nel canale di origine
+            client.add_event_handler(forward_message, events.NewMessage(chats=source_entity))
+
+        # Avvia il client
+        await client.run_until_disconnected()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    import asyncio
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
